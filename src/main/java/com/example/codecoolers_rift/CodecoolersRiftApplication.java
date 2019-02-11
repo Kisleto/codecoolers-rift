d80a9f06b48f99843fa9a800128bdfdb37f4a8a4
Viktor Zsolnai <zsviktor45@gmail.com>
2019-02-11 14:20:25 +0100

    Initial commit

---
+++ src/main/java/com/example/codecoolers_rift/CodecoolersRiftApplication.java
@@ -0,0 +1,14 @@
+package com.example.codecoolers_rift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodecoolersRiftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodecoolersRiftApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/example/codecoolers_rift/CodecoolersRiftApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.codecoolers_rift;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CodecoolersRiftApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
