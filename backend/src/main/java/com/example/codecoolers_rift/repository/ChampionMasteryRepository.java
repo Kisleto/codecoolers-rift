bd68745206d7e73aabadae6f50407659b4ef4cee
peterSzalma <petiszalma@gmail.com>
2019-02-28 13:23:56 +0100

    champion mastery data saved to database

--- backend/src/main/java/com/example/codecoolers_rift/repository/ChampionMasteryRepository.java
+++ backend/src/main/java/com/example/codecoolers_rift/repository/ChampionMasteryRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ChampionMasteryRepository extends CrudRepository<ChampionMastery, Long> {
+public interface ChampionMasteryRepository extends JpaRepository<ChampionMastery, Long> {
+    boolean existsAllByChampionIdIsNotNull();
 }
