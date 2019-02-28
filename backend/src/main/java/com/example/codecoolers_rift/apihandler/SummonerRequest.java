31578c7395d17f1880ea651fd5a4079132337f08
Imre Maczkó <immaczko@gmail.com>
2019-02-28 13:02:55 +0100

    API key changed

--- backend/src/main/java/com/example/codecoolers_rift/apihandler/SummonerRequest.java
+++ backend/src/main/java/com/example/codecoolers_rift/apihandler/SummonerRequest.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class SummonerRequest {
 
 
-    private static String key = "RGAPI-559dc065-b11e-447c-899a-fb12094c43b8";
+    private static String key = "RGAPI-f017da10-509d-4573-acd0-81d845a54a53";
 
 
 
