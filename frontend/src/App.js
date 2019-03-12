ad65c7080cb03952b1c21293b9407162783eb451
Kisleto <zsviktor45@gmail.com>
2019-03-12 14:58:09 +0100

    implementing login...

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/header";
 import {HashRouter} from "react-router-dom";
 import {withStyles} from "@material-ui/core";
 import PropTypes from "prop-types";
+import {Navbar, Button} from 'react-bootstrap';
 
 
 const styles = theme => ({
@@ -18,21 +19,81 @@ const styles = theme => ({
 });
 
 class App extends Component {
+    goTo(route) {
+        this.props.history.replace(`/${route}`)
+    }
+
+    login() {
+        this.props.auth.login();
+    }
+
+    logout() {
+        this.props.auth.logout();
+    }
+
+    componentDidMount() {
+        const {renewSession} = this.props.auth;
+
+        if (localStorage.getItem('isLoggedIn') === 'true') {
+            renewSession();
+        }
+    }
 
 
     render() {
-
         return (
+            <div>
+                <Navbar fluid>
+                    <Navbar.Header>
+                        <Navbar.Brand>
+                            <a href="#">Auth0 - React</a>
+                        </Navbar.Brand>
+                        <Button
+                            bsStyle="primary"
+                            className="btn-margin"
+                            onClick={this.goTo.bind(this, 'home')}
+                        >
+                            Home
+                        </Button>
+                        {
+                            (
+                                <Button
+                                    id="qsLoginBtn"
+                                    bsStyle="primary"
+                                    className="btn-margin"
+                                    onClick={this.login.bind(this)}
+                                >
+                                    Log In
+                                </Button>
+                            )
+                        }
+                        {
+                            (
+                                <Button
+                                    id="qsLogoutBtn"
+                                    bsStyle="primary"
+                                    className="btn-margin"
+                                    onClick={this.logout.bind(this)}
+                                >
+                                    Log Out
+                                </Button>
+                            )
+                        }
+                    </Navbar.Header>
+                </Navbar>
                 <HashRouter>
                     <div>
                         <Header/>
                     </div>
                 </HashRouter>
+            </div>
 
-            )
-        }
 
+        )
+    }
 }
+
+
 
 App.propTypes = {
     classes: PropTypes.object.isRequired,
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,11 +4,14 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import {makeMainRoutes} from './routes';
 
-ReactDOM.render((
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>), document.getElementById('root'));
+const routes = makeMainRoutes();
+
+ReactDOM.render(
+    routes,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
